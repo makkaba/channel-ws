9a2140806b83aa0dd113869aa23d6c000ff3995e
EC2 Default User <ec2-user@ip-172-31-14-70.ap-northeast-2.compute.internal>
2017-07-31 02:35:01 +0000

    firebase를 추가한다

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./channel-8e58c-firebase-adminsdk-umm8k-410d7f26c1.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://channel-8e58c.firebaseio.com"
+});
+
+var db = admin.database();
+var roomRef = db.ref('room');
+roomRef.once("value", (snapshot)=>{
+  console.log(snapshot.val());
+});
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -12,11 +27,21 @@ app.get('/', (req, res)=>{
 
 io.on('connection', (socket)=>{
   console.log('a user connected');
+  
+  /*
+  if(//디비에 room 정보가 없으면){
+    //룸 정보를 생성한다
+  }else{
+    //있으면 존재하는 룸 중에 가장 최근 방으로 연결시킨다.
 
-  socket.join('room 237', ()=>{
+  }
+  */
+  var roomUID = generateId(13);
+
+  socket.join(roomUID, ()=>{
     let rooms = Object.keys(socket.rooms);
-    console.log(rooms); // [ <socket.id>, 'room 237' ]
-    io.sockets.in('room 237').emit('message', {message:'새로운 손님이 입장했습니다.', userName:''});
+    console.log("rooms:",rooms); // [ <socket.id>, 'room 237' ]
+    io.sockets.in(roomUID).emit('message', {message:'새로운 손님이 입장했습니다.', userName:''});
   });
   // socket.on('add user', function(userName){
   //   console.log('add user!!'+userName);
@@ -24,10 +49,19 @@ io.on('connection', (socket)=>{
   //     userName: userName
   //   });
   // });
+  socket.on('register', (data)=>{
 
+    /*
+    db에 채널ID를 생성하여 넣고
+    소켓ID를 넣고 
+    유저 아이디를 넣는다.
+    */
+    console.log("socketID:"+socket.id+"\n roomID:"+ roomUID);
+
+  });
   socket.on('message', (data)=>{
     console.log("msg", data);
-    io.sockets.in('room 237').emit('message', data);
+    io.sockets.in(roomUID).emit('message', data);
   });
   socket.on('disconnect', ()=>{
     console.log('a user disconnected');
@@ -38,6 +72,16 @@ io.on('connection', (socket)=>{
 http.listen(8080, ()=>{
   console.log('listening on *:8080');
 });
+
+function generateId(size){
+  var result = "";
+  var pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var length = pool.length + 1;
+  for(var i = 0; i < size; i++) {
+      result += pool.charAt(Math.floor(Math.random() * (length)));
+  }
+  return result;
+}
     
 /*
 var http = require('http');
